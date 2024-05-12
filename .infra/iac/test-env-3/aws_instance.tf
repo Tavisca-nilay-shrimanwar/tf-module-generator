a5336c68f2c6302288ab755cc8bd28ba9921408a
Tavisca-nilay-shrimanwar <124360321+Tavisca-nilay-shrimanwar@users.noreply.github.com>
2024-05-12 23:25:21 +0530

    Update aws_instance.tf

--- .infra/iac/test-env-3/aws_instance.tf
+++ .infra/iac/test-env-3/aws_instance.tf
@@ -3,13 +3,13 @@ module "ec2" {
   for_each = var.aws_instance
   ami = each.value["ami"]
   instance_type = each.value["instance_type"]
-  capacity_reservation_specification = each.value["capacity_reservation_specification"]
-  cpu_options = try(each.value["cpu_options"], null)
-  credit_specification = each.value["credit_specification"]
+  capacity_reservation_specification =  try(each.value["capacity_reservation_specification"], [])
+  cpu_options = try(each.value["cpu_options"], [])
+  credit_specification = try(each.value["credit_specification"], [])
   key_name = each.value["key_name"]
   vpc_security_group_ids = each.value["vpc_security_group_ids"]
-  metadata_options = each.value["metadata_options"]
-  private_dns_name_options = try(each.value["private_dns_name_options"], null)
+  metadata_options = try(each.value["metadata_options"], [])
+  private_dns_name_options = try(each.value["private_dns_name_options"], [])
   subnet_id = each.value["subnet_id"]
-  root_block_device = each.value["root_block_device"]
+  root_block_device = try(each.value["root_block_device"], [])
 }
